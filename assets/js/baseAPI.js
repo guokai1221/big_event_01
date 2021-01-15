13243752ae3d60fdbb003864dc115eeac4af9091
15635603721 <1904113496@qq.com>
2021-01-15 15:42:45 +0800

    完成登录注册模块代码注释

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,7 +5,10 @@ var baseURL = 'http://api-breakingnews-web.itheima.net'
 //生成环境服务器地址
 var baseURL = 'http://api-breakingnews-web.itheima.net'
 
-//拦截所有ajax请求  在ajax请求之前做的操作
+
+//注意 每次调用 $.get() $.post() $.ajax()的时候
+//会先调用 ajaxPrefilter 这个函数
+//在这个函数中 可以呢到我们给ajax提供的配置对象 
 //在发送ajax请求之前 对url地址进行处理 这样方便管理
 $.ajaxPrefilter(function (options) {
     options.url = baseURL + options.url;
